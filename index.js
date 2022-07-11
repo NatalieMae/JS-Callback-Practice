be50e2b57bc6ebc6f18579952b35e2c0c002ed92
Natalie <nataliemae263@gmail.com>
2022-07-11 15:56:00 -0500

    all done

--- index.js
+++ index.js
@@ -2,51 +2,7 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-
-function moveCharacter() {
-    if(direction === 'west') {
-        x = x - 1
-    }
-    if(direction === 'north') {
-        y = y + 1
-    }
-    if(direction === 'east') {
-        x = x + 1
-    }
-    if(direction === 'south') {
-        y = y - 1
-    }
-
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}
-
-
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft') {
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp') {
-        direction == 'north'
-    }
-    if (e.key === 'ArrowRight') {
-        direction == 'east'
-    }
-    if(e.key === 'ArrowDown') {
-        direction === 'south'
-    }
-})
-
-move(character).to(100, 250)
-
-setInterval(moveCharacter, 1)
-
+move(character).withArrowKeys(100, 250)
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
@@ -55,3 +11,31 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+let x = 100;
+let y = 250;
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+// move(character).to(100, 250)
+
+setInterval(moveCharacter(), 1)
+
